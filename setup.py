42d9a91dedbbe2ea900eadaaca64f00ded1666ee
Ashwin Srinath <ashwinsrnth@gmail.com>
2014-10-14 13:19:34 -0400

    Adding cuda support in setup.py

--- setup.py
+++ setup.py
@@ -1,63 +1,104 @@
-#!/usr/bin/env python
+import os
+from os.path import join as pjoin
+from setuptools import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import subprocess
+import numpy
 
-#$ python setup.py build_ext --inplace
+# cusp and thrust 
+INCLUDE_DIRS = ['/home/atrikut/local/cusplibrary', '/home/atrikut/local/thrust/']
+LIBRARY_DIRS = []
+LIBRARIES    = []
 
-from numpy.distutils.command import build_src
+# PETSc
+import os
+PETSC_DIR  = os.environ['PETSC_DIR']
+PETSC_ARCH = os.environ.get('PETSC_ARCH', '')
+from os.path import join, isdir
+if PETSC_ARCH and isdir(join(PETSC_DIR, PETSC_ARCH)):
+    INCLUDE_DIRS += [join(PETSC_DIR, PETSC_ARCH, 'include'),
+                     join(PETSC_DIR, 'include')]
+    LIBRARY_DIRS += [join(PETSC_DIR, PETSC_ARCH, 'lib')]
+else:
+    if PETSC_ARCH: pass # XXX should warn ...
+    INCLUDE_DIRS += [join(PETSC_DIR, 'include')]
+    LIBRARY_DIRS += [join(PETSC_DIR, 'lib')]
+LIBRARIES += [#'petscts', 'petscsnes', 'petscksp',
+              #'petscdm', 'petscmat',  'petscvec',
+              'petsc']
 
-# a bit of monkeypatching ...
-import Cython.Compiler.Main
-build_src.Pyrex = Cython
-build_src.have_pyrex = True
-def have_pyrex():
-    import sys
-    try:
-        import Cython.Compiler.Main
-        sys.modules['Pyrex'] = Cython
-        sys.modules['Pyrex.Compiler'] = Cython.Compiler
-        sys.modules['Pyrex.Compiler.Main'] = Cython.Compiler.Main
-        return True
-    except ImportError:
-        return False
-build_src.have_pyrex = have_pyrex
+# PETSc for Python
+import petsc4py
+INCLUDE_DIRS += [petsc4py.get_include()]
 
-def configuration(parent_package='',top_path=None):
-    INCLUDE_DIRS = ['/home/atrikut/local/cusplibrary', '/home/atrikut/local/thrust/']
-    LIBRARY_DIRS = []
-    LIBRARIES    = []
+# CUDA
+from setup_cuda import locate_cuda
+CUDA = locate_cuda()
+LIBRARY_DIRS += [CUDA['lib64']]
+LIBRARIES += ['cudart']
+INCLUDE_DIRS += [CUDA['include']]
 
-    # PETSc
-    import os
-    PETSC_DIR  = os.environ['PETSC_DIR']
-    PETSC_ARCH = os.environ.get('PETSC_ARCH', '')
-    from os.path import join, isdir
-    if PETSC_ARCH and isdir(join(PETSC_DIR, PETSC_ARCH)):
-        INCLUDE_DIRS += [join(PETSC_DIR, PETSC_ARCH, 'include'),
-                         join(PETSC_DIR, 'include')]
-        LIBRARY_DIRS += [join(PETSC_DIR, PETSC_ARCH, 'lib')]
-    else:
-        if PETSC_ARCH: pass # XXX should warn ...
-        INCLUDE_DIRS += [join(PETSC_DIR, 'include')]
-        LIBRARY_DIRS += [join(PETSC_DIR, 'lib')]
-    LIBRARIES += [#'petscts', 'petscsnes', 'petscksp',
-                  #'petscdm', 'petscmat',  'petscvec',
-                  'petsc']
+# Obtain the numpy include directory:
+import numpy
+numpy_include = numpy.get_include() 
+INCLUDE_DIRS += [numpy_include]
 
-    # PETSc for Python
-    import petsc4py
-    INCLUDE_DIRS += [petsc4py.get_include()]
+ext = Extension('GPUArray',
+                sources = ['GPUArray.pyx'],
+                depends = ['GPUArrayimpl.h'],
+                include_dirs = INCLUDE_DIRS + [os.curdir],
+                libraries = LIBRARIES,
+                library_dirs = LIBRARY_DIRS,
+                runtime_library_dirs = LIBRARY_DIRS,
+                extra_compile_args = {'gcc': [],
+                                      'nvcc': ['-arch=sm_21', '--ptxas-options=-v', '-c', '--compiler-options', "'-fPIC'"]}
+                )
+def customize_compiler_for_nvcc(self):
+    """inject deep into distutils to customize how the dispatch
+    to gcc/nvcc works.
 
-    # Configuration
-    from numpy.distutils.misc_util import Configuration
-    config = Configuration('', parent_package, top_path)
-    config.add_extension('GPUArray',
-                         sources = ['GPUArray.pyx'],
-                         depends = ['GPUArrayimpl.h'],
-                         include_dirs=INCLUDE_DIRS + [os.curdir],
-                         libraries=LIBRARIES,
-                         library_dirs=LIBRARY_DIRS,
-                         runtime_library_dirs=LIBRARY_DIRS)
-    return config
+    If you subclass UnixCCompiler, it's not trivial to get your subclass
+    injected in, and still have the right customizations (i.e.
+    distutils.sysconfig.customize_compiler) run on it. So instead of going
+    the OO route, I have this. Note, it's kindof like a wierd functional
+    subclassing going on."""
 
-if __name__ == "__main__":
-    from numpy.distutils.core import setup
-    setup(**configuration(top_path='').todict())
+    # tell the compiler it can processes .cu
+    self.src_extensions.append('.cu')
+
+    # save references to the default compiler_so and _comple methods
+    default_compiler_so = self.compiler_so
+    super = self._compile
+
+    # now redefine the _compile method. This gets executed for each
+    # object but distutils doesn't have the ability to change compilers
+    # based on source extension: we add it.
+    def _compile(obj, src, ext, cc_args, extra_postargs, pp_opts):
+        if os.path.splitext(src)[1] == '.cu':
+            # use the cuda for .cu files
+            self.set_executable('compiler_so', CUDA['nvcc'])
+            # use only a subset of the extra_postargs, which are 1-1 translated
+            # from the extra_compile_args in the Extension class
+            postargs = extra_postargs['nvcc']
+        else:
+            postargs = extra_postargs['gcc']
+
+        super(obj, src, ext, cc_args, postargs, pp_opts)
+        # reset the default compiler_so, which we might have changed for cuda
+        self.compiler_so = default_compiler_so
+
+    # inject our redefined _compile method into the class
+    self._compile = _compile
+
+# run the customize_compiler:
+class custom_build_ext(build_ext):
+    def build_extensions(self):
+        customize_compiler_for_nvcc(self.compiler)
+        build_ext.build_extensions(self)
+
+setup(name = 'GPUArray',
+      ext_modules = [ext],
+      cmdclass = {'build_ext': custom_build_ext},
+      zip_safe = False)
+
