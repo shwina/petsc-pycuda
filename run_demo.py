4cf5e91e7461d476909b05ca2c1c644747045efa
Ashwin Srinath <ashwinsrnth@gmail.com>
2014-10-14 18:36:35 -0400

    added new test in run_demo

--- run_demo.py
+++ run_demo.py
@@ -7,7 +7,18 @@ v.setSizes(4)
 v.setType('cusp')
 v.set(2.0)
 
+print 'Original PETSc Vec:'
 print v.array
+
 G = GPUArray.getGPUArray(v)
+print 'GPUArray constructed from PETSc Vec:'
 print G.get()
+
+print 'Modified GPUArray:'
+G.fill(4.0)
+print G
+
+print 'PETSc Vec changes too:'
+print v.array
+
 v.destroy()
