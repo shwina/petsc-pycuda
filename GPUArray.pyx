7df29607abb96cc123712d66738bfa823a62594d
Ashwin Srinath <ashwinsrnth@gmail.com>
2014-10-17 07:32:14 -0400

    changed uint64_t to uintptr_t

--- GPUArray.pyx
+++ GPUArray.pyx
@@ -4,17 +4,16 @@ import pycuda.gpuarray as gpuarray
 from pycuda import autoinit
 import numpy as np
 
+from libc.stdint cimport uintptr_t
+
 cdef extern from "GPUArrayimpl.h":
     int VecGetGPUArray(PetscVec vec, double** array)
     int VecUpdateGPUStatus(PetscVec vec)
 
-cdef extern from "stdint.h":
-    ctypedef unsigned long long uint64_t
-
 def getGPUArray(Vec V):
     cdef double *array
     VecGetGPUArray(V.vec, &array)
-    G = gpuarray.GPUArray(V.getSize(), dtype=np.float64, allocator=None, gpudata=int(<uint64_t>array))
+    G = gpuarray.GPUArray(V.getSize(), dtype=np.float64, allocator=None, gpudata=int(<uintptr_t>array))
     return G
 
 def updateVecStatus(Vec V):
