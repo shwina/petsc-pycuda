7e41f194d65524b19b54605ca95a50d87d6dd6e6
Ashwin Srinath <ashwinsrnth@gmail.com>
2014-10-21 11:28:42 -0400

    Use petsc's GetCUDAArray instead of ours

--- GPUArray.pyx
+++ GPUArray.pyx
@@ -6,13 +6,15 @@ import numpy as np
 
 from libc.stdint cimport uintptr_t
 
+cdef extern from "petsccusp.h":
+    int VecCUSPGetCUDAArray(PetscVec Vec, double** array)
+
 cdef extern from "GPUArrayimpl.h":
-    int VecGetGPUArray(PetscVec vec, double** array)
     int VecUpdateGPUStatus(PetscVec vec)
 
 def getGPUArray(Vec V):
     cdef double *array
-    VecGetGPUArray(V.vec, &array)
+    VecCUSPGetCUDAArray(V.vec, &array)
     G = gpuarray.GPUArray(V.getLocalSize(), dtype=np.float64, allocator=None, gpudata=int(<uintptr_t>array))
     return G
 
